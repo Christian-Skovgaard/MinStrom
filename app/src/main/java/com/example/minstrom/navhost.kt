0ba99407edb86a559497fe06003dd1d594ca0e53
unicornfarm5 <linea.moltved@gmail.com>
2025-04-02 11:36:38 +0200

    created navigation in app

--- app/src/main/java/com/example/minstrom/navhost.kt
+++ app/src/main/java/com/example/minstrom/navhost.kt
@@ -1,2 +1,19 @@
 package com.example.minstrom
 
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHost
+import androidx.navigation.compose.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun AppNavigation() {
+        val navController = rememberNavController()
+        NavHost(navController = navController, startDestination = "start-screen") {
+                composable("start-screen") {
+                       ScreenLinea()
+                }
+        }
+}
