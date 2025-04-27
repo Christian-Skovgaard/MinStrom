fe7c44a9b4c5bb8a87123f25154899326a82d125
Christian-Skovgaard <cellochr@gmail.com>
2025-04-27 22:52:59 +0200

    virker og kan konvertere til test objekt

--- app/src/main/java/com/example/minstrom/MainActivity.kt
+++ app/src/main/java/com/example/minstrom/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            getData()
+            getDeviceList()
             MinstromTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
--- app/src/main/java/com/example/minstrom/firebase.kt
+++ app/src/main/java/com/example/minstrom/firebase.kt
@@ -3,28 +3,37 @@ package com.example.minstrom
 import android.util.Log
 import com.google.firebase.Firebase
 import com.google.firebase.firestore.firestore
-import com.google.firebase.firestore.ktx.firestore
-import kotlin.reflect.typeOf
+import com.google.firebase.firestore.toObject
 
-fun getData () {
+data class TestObj (
+    val name:String = "",
+    //val associatedUsers:List<String>,
+    val notificationEnable:Boolean = false
+)
+
+fun getDeviceList () {
 
     val db = Firebase.firestore
 
-    val docRef = db.collection("devices").document("jd9bhcf0iQW8x0zpXCPw")
+    val docRef = db.collection("devices")
+    //.document("jd9bhcf0iQW8x0zpXCPw")
 
     docRef.get()
-        .addOnSuccessListener { document ->
-            if (document != null) {
-                Log.d("Vask", "DocumentSnapshot data: ${document.data}")
-                Log.d("Vask", "DocumentSnapshot data: ${document.data}")
+        .addOnSuccessListener { documents ->
+            if (documents != null) {
+                for (document in documents) {
+                    Log.d("Vask", "DocumentSnapshot data: ${document}")
+                    val device = document.toObject<TestObj>()
+                    Log.d("Vask", "Device: ${device}")
+                }
+
+                //Log.d("Vask", "DocumentSnapshot data: ${document}")
             } else {
-                Log.d("Vask", "No such document")
+                Log.d("Vask", "List is empty")
             }
         }
         .addOnFailureListener { exception ->
             Log.d("Vask", "get failed with ", exception)
         }
 }
-
-
 
