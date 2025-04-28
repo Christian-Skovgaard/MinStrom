023b587e904977879a227fe47a23281d04f33002
Christian-Skovgaard <cellochr@gmail.com>
2025-04-28 11:09:12 +0200

    made a better time system for Device class

--- app/src/main/java/com/example/minstrom/MainActivity.kt
+++ app/src/main/java/com/example/minstrom/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.minstrom
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -17,15 +18,31 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.compose.composable
 import com.example.minstrom.ui.theme.MinstromTheme
+import java.time.LocalTime
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.minutes
+import kotlin.time.DurationUnit
+import kotlin.time.toDuration
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            getDeviceList()
+            val firebase: Firebase = Firebase()
+            firebase.getDeviceList()
             val navController = rememberNavController()
             Navhost(navController = navController)
+            testTime()
         }
     }
+}
+
+fun testTime () {
+    var time: LocalTime = LocalTime.of(15,30)
+
+    var string = "16:37"
+    var newTime = LocalTime.parse(string)
+
+    Log.d("VERYVISABLETAG",newTime.toString())
 }
